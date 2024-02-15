ef822871aa16e5ef5b60e3f67acd1bb6e274c517
Milo <zaffirim@gmail.com>
2024-02-15 12:42:24 +0100

    feat: attempt 1 implement timer

--- assets/js/script-page2.js
+++ assets/js/script-page2.js
@@ -89,9 +89,163 @@ let currentQuestion = 1;
 let totalScore = 0;
 const totalQuestions = questions.length;
 
-let timeLeft = 0;
+/* let timeLeft = 0; */
 let questionTimer;
 
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green",
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD,
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD,
+  },
+};
+
+let TIME_LIMIT = 30;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.querySelector("#timer").innerHTML = `
+
+   <div class="base-timer">
+        <svg
+          class="base-timer__svg"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/1000/svg"
+        >
+          <g class="base-timer__circle">
+            <circle
+              class="base-timer__path-elapsed"
+              cx="50"
+              cy="50"
+              r="45"
+            ></circle>
+            <path
+              id="base-timer-path-remaining"
+              stroke-dasharray="283"
+              class="base-timer__path-remaining ${remainingPathColor}"
+              d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+            ></path>
+          </g>
+        </svg>
+        <span id="base-timer-label" class="base-timer__label">${formatTime(
+          timeLeft
+        )}</span>
+      </div>
+
+`;
+
+/* window.addEventListener("load", () => {
+  const startBtn = document.getElementById("start-btn");
+  const timeInput = document.getElementById("time-input");
+  //   console.log(startBtn, timeInput);
+  timeInput.onchange = () => {
+    TIME_LIMIT = timeInput.value;
+    timeInput.value = "";
+  };
+  startBtn.onclick = () => {
+    TIME_LIMIT === 0 ? alert("Add time") : startTimer();
+  };
+}); */
+
+function startTimer() {
+  console.log("value");
+
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML =
+      formatTime(timeLeft);
+
+    setCircleDasharray();
+
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+  TIME_LIMIT = 0;
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}
+    `;
+  }
+
+  return ` ${minutes}:${seconds}`;
+}
+
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+// FUNZIONI CHE MI FANNO RIMPICCIOLIRE LA BARRA
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+} 
+
+
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+const createTimer = () => {
+  startTimer();
+  onTimesUp();
+  setCircleDasharray();
+  
+}
+/* 
 //FUNZIONE CHE, ALLO SCADERE DEL TEMPO, CREA UN NUOVO EVENTO 'CLICK' SUL BOTTONE 'NEXT' E PASSA ALLA SCHERMATA SUCCESSIVA
 
 const startQuestionTimer = () => {
@@ -117,7 +271,7 @@ const runTimer = () => {
 const updateTimerDisplay = (seconds) => {
   const timerDisplay = document.querySelector(".timer");
   timerDisplay.innerHTML = `<p>Tempo rimanente: ${seconds} secondi</p>`;
-};
+}; */
 
 /* FUNZIONE CHE SVUOTA IL DIV 'quizSpace' sapzio in cui ci sono le risposte, sennò ci sarebbero le risposte della domanda prima e quelle della domanda dopo */
 
@@ -184,7 +338,7 @@ const eventHandler = (event) => {
     createQuestionText();
     createAnswerOptions();
     createCurrentQuestiontext();
-    startQuestionTimer();
+    /* startQuestionTimer(); */
   } else {
     clearInterval(questionTimer);
     localStorage.setItem("totalScore", totalScore.toString());
@@ -197,7 +351,7 @@ const init = (event) => {
   createQuestionText();
   createAnswerOptions();
   createCurrentQuestiontext();
-  startQuestionTimer();
+  /* startQuestionTimer(); */
 };
 
 nextButton.addEventListener("click", eventHandler);
